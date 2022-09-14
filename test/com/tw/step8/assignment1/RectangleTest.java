1934d47b0fbc6567442c6a89de34dfda5e51d1e8
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-09-14 13:08:17 +0530

    Added factory method for createRectangle and createSquare in Rectangle

--- test/com/tw/step8/assignment1/RectangleTest.java
+++ test/com/tw/step8/assignment1/RectangleTest.java
@@ -8,13 +8,19 @@ class RectangleTest {
 
   @Test
   void shouldReturnAreaOfRectangle() {
-    Rectangle rectangle = new Rectangle(10.0,10.0);
+    Rectangle rectangle = Rectangle.createRectangle(10.0,10.0);
+    Rectangle square = Rectangle.createSquare(20.0);
+
     assertEquals(100.0,rectangle.area());
+    assertEquals(400.0,square.area());
   }
 
   @Test
   void shouldReturnPerimeterOfRectangle() {
-    Rectangle rectangle = new Rectangle(10.0,10.0);
+    Rectangle rectangle = Rectangle.createRectangle(10.0,10.0);
+    Rectangle square = Rectangle.createSquare(20.0);
+
     assertEquals(40.0, rectangle.perimeter());
+    assertEquals(80.0, square.perimeter());
   }
 }
